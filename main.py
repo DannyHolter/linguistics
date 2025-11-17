1be0a01d04dde1234fb5618719cdb2e077edb6ae
DannyHolter <dannyholter3206@gmail.com>
2025-11-17 18:22:58 -0500

    Add files via upload
    removed redundant code

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ def generate(stuff):
             generate(item)
 
 def syntaxtree(rule, depth=0):
-    print("     "*depth + "(" + str(rule[0]))
-    for child in rule[1]:
+    label = rule[0]
+    children = rule[1]
+    print("     "*depth + "(" + str(label))
+    for child in children:
         if isinstance(child, tuple):
             syntaxtree(child, depth+1)
         else:
@@ -28,10 +30,7 @@ def wordtree(rule, depth=0):
         if isinstance(child, tuple):
             wordtree(child, depth+1)
         else:
-            if isinstance(child, str):
-                print("     "*(depth+1) + child + " " + str(random.choice(lex[child])))
-            else:
-                print("     "*(depth+1) + child)
+            print("     "*(depth+1) + child + " " + str(random.choice(lex[child])))
     print("     "*depth +")")
 
 wordtree(form)
